e8844a2cf84a0e00629098bac778a4eb02433741
sandra gutierrez <sandraivonneans18@gmail.com>
2024-04-22 19:35:11 -0600

    version2

--- MainSuma.java
+++ MainSuma.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 public class MainSuma {
     public static void main(String[] args) {
         Suma n1 = new Suma();
-        n1.a= 17;
-        n1.b= 46;
+        Scanner num= new Scanner (System.in);
+        System.out.println("Suma de 2 numeros");
+        System.out.println("Ingrese un numero:");
+        int a= num.nextInt();
+        n1.a=a;
+        System.out.println("Ingrese segundo1 numero:");
+        int b= num.nextInt();
+        n1.b=b;
+        System.out.println("El resultado es:");
         System.out.println(n1.sumar());
-        
+    
     }
 }
