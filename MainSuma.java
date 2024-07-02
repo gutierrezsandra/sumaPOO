e3df09c5dff50f8d70f5ffe81e84445f797cbe48
gutierrezsandra <167218740+gutierrezsandra@users.noreply.github.com>
2024-07-02 14:31:39 -0600

    Update MainSuma.java

--- MainSuma.java
+++ MainSuma.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
+
 public class MainSuma {
     public static void main(String[] args) {
-        Suma n1 = new Suma();
-        Scanner num= new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Suma de 2 numeros");
         System.out.println("Ingrese un numero:");
-        int a= num.nextInt();
-        n1.a=a;
-        System.out.println("Ingrese segundo1 numero:");
-        int b= num.nextInt();
-        n1.b=b;
-        System.out.println("El resultado es:");
-        System.out.println(n1.sumar());
-    
+        int a = scanner.nextInt();
+        System.out.println("Ingrese segundo numero:");
+        int b = scanner.nextInt();
+        
+        int result = a + b;
+        System.out.println("El resultado es: " + result);
     }
 }
